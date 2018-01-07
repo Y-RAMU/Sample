9977b8cbc4ee1ad8ea183d609d06581b80e32895
Y-RAMU <34812260+Y-RAMU@users.noreply.github.com>
2018-01-08 00:10:20 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -4,7 +4,7 @@ javascript:
 	{
 		j=['https://sgimera.github.io/mai_RatingAnalyzer/scripts/mai_inner_level.js',
 		   'https://sgimera.github.io/mai_RatingAnalyzer/scripts/calc_rating.js',
-		   'https://sgimera.github.io/mai_RatingAnalyzer/scripts/maiRatingAnalyzer_body.js'];
+		   'https://rawgit.com/Y-RAMU/Sample/master/body.js'];
 		f=function(s,u)
 		{
 			if(j.length==0)
