34d81a90fe7ae60484d7ef2a2b7d9b3cdd3ecf28
Y-RAMU <34812260+Y-RAMU@users.noreply.github.com>
2018-03-11 23:39:22 +0900

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,8 @@ javascript:
 	{
 		j=['https://sgimera.github.io/mai_RatingAnalyzer/scripts/mai_inner_level_milk2.js',
 		   'https://sgimera.github.io/mai_RatingAnalyzer/scripts/calc_rating_test.js',
-		   'https://sgimera.github.io/mai_RatingAnalyzer/scripts/maiRatingAnalyzer_body.js'];
+		   //'https://sgimera.github.io/mai_RatingAnalyzer/scripts/maiRatingAnalyzer_body.js'
+		   'https://cdn.rawgit.com/Y-RAMU/Sample/ed93109a/body.js'];
 		f=function(s,u)
 		{
 			if(j.length==0)
